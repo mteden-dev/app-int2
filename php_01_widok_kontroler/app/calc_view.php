4c660c45a9ada2657ceacc2c5017819280158f3a
unknown <banaszprzemyslaw@gmail.com>
2024-03-24 19:57:39 +0100

    dodanie slidera

--- php_01_widok_kontroler/app/calc_view.php
+++ php_01_widok_kontroler/app/calc_view.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="utf-8" />
     <title>Kalkulator kredytowy</title>
+	<style>
+		.slider {
+			width: 300px;
+		}
+	</style>
 </head>
 <body>
 
@@ -11,11 +16,17 @@
     <label for="id_kwota">Kwota kredytu: </label>
     <input id="id_kwota" type="text" name="kwota" value="<?php if (isset($kwota)) print($kwota); ?>" /><br />
     <label for="id_lata">Okres kredytu (w latach): </label>
-    <input id="id_lata" type="text" name="lata" value="<?php if (isset($lata)) print($lata); ?>" /><br />
+    <input id="id_lata" type="range" name="lata" class="slider" min="1" max="30" value="<?php if (isset($lata)) print($lata); else print(1); ?>" oninput="updateLata(this.value)" /> <span id="lataValue"><?php if (isset($lata)) print($lata); else print(1); ?></span><br />
     <label for="id_oprocentowanie">Oprocentowanie (w %): </label>
     <input id="id_oprocentowanie" type="text" name="oprocentowanie" value="<?php if (isset($oprocentowanie)) print($oprocentowanie); ?>" /><br />
     <input type="submit" value="Oblicz ratę" />
 </form>
+
+<script>
+	function updateLata(val) {
+		document.getElementById('lataValue').innerHTML = val;
+	}
+</script>
 
 <?php
 // wyświetlenie listy błędów, jeśli istnieją
