555a48a8bf9afab74948be535ed580b154549735
unknown <banaszprzemyslaw@gmail.com>
2024-03-24 19:37:50 +0100

    Kalkulator Kredytowy

--- php_01_widok_kontroler/app/calc.php
+++ php_01_widok_kontroler/app/calc.php
@@ -1,73 +1,55 @@
 <?php
-// KONTROLER strony kalkulatora
+// KONTROLER kalkulatora kredytowego
 require_once dirname(__FILE__).'/../config.php';
 
-// W kontrolerze niczego nie wysyła się do klienta.
-// Wysłaniem odpowiedzi zajmie się odpowiedni widok.
-// Parametry do widoku przekazujemy przez zmienne.
-
 // 1. pobranie parametrów
-
-$x = $_REQUEST ['x'];
-$y = $_REQUEST ['y'];
-$operation = $_REQUEST ['op'];
+$kwota = $_REQUEST['kwota'];
+$lata = $_REQUEST['lata'];
+$oprocentowanie = $_REQUEST['oprocentowanie'];
 
 // 2. walidacja parametrów z przygotowaniem zmiennych dla widoku
+$messages = array();
 
 // sprawdzenie, czy parametry zostały przekazane
-if ( ! (isset($x) && isset($y) && isset($operation))) {
-	//sytuacja wystąpi kiedy np. kontroler zostanie wywołany bezpośrednio - nie z formularza
-	$messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+if (!(isset($kwota) && isset($lata) && isset($oprocentowanie))) {
+    $messages[] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
 }
 
 // sprawdzenie, czy potrzebne wartości zostały przekazane
-if ( $x == "") {
-	$messages [] = 'Nie podano liczby 1';
+if ($kwota == "") {
+    $messages[] = 'Nie podano kwoty kredytu';
 }
-if ( $y == "") {
-	$messages [] = 'Nie podano liczby 2';
+if ($lata == "") {
+    $messages[] = 'Nie podano okresu kredytu';
+}
+if ($oprocentowanie == "") {
+    $messages[] = 'Nie podano oprocentowania';
 }
 
-//nie ma sensu walidować dalej gdy brak parametrów
-if (empty( $messages )) {
-	
-	// sprawdzenie, czy $x i $y są liczbami całkowitymi
-	if (! is_numeric( $x )) {
-		$messages [] = 'Pierwsza wartość nie jest liczbą całkowitą';
-	}
-	
-	if (! is_numeric( $y )) {
-		$messages [] = 'Druga wartość nie jest liczbą całkowitą';
-	}	
-
+// nie ma sensu walidować dalej gdy brak parametrów
+if (empty($messages)) {
+    // sprawdzenie, czy $kwota, $lata i $oprocentowanie są liczbami
+    if (!is_numeric($kwota)) {
+        $messages[] = 'Kwota kredytu nie jest liczbą';
+    }
+    if (!is_numeric($lata)) {
+        $messages[] = 'Okres kredytu nie jest liczbą';
+    }
+    if (!is_numeric($oprocentowanie)) {
+        $messages[] = 'Oprocentowanie nie jest liczbą';
+    }
 }
 
 // 3. wykonaj zadanie jeśli wszystko w porządku
+if (empty($messages)) {
+    $kwota = floatval($kwota);
+    $lata = intval($lata);
+    $oprocentowanie = floatval($oprocentowanie) / 100;
 
-if (empty ( $messages )) { // gdy brak błędów
-	
-	//konwersja parametrów na int
-	$x = intval($x);
-	$y = intval($y);
-	
-	//wykonanie operacji
-	switch ($operation) {
-		case 'minus' :
-			$result = $x - $y;
-			break;
-		case 'times' :
-			$result = $x * $y;
-			break;
-		case 'div' :
-			$result = $x / $y;
-			break;
-		default :
-			$result = $x + $y;
-			break;
-	}
+    $miesiace = $lata * 12;
+    $rata = ($kwota * $oprocentowanie / 12) / (1 - pow(1 + $oprocentowanie / 12, -$miesiace));
+    $rata = round($rata, 2);
 }
 
 // 4. Wywołanie widoku z przekazaniem zmiennych
-// - zainicjowane zmienne ($messages,$x,$y,$operation,$result)
-//   będą dostępne w dołączonym skrypcie
 include 'calc_view.php';
--- php_01_widok_kontroler/app/calc_view.php
+++ php_01_widok_kontroler/app/calc_view.php
@@ -2,42 +2,37 @@
 <!DOCTYPE HTML>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
 <head>
-<meta charset="utf-8" />
-<title>Kalkulator</title>
+    <meta charset="utf-8" />
+    <title>Kalkulator kredytowy</title>
 </head>
 <body>
 
 <form action="<?php print(_APP_URL);?>/app/calc.php" method="post">
-	<label for="id_x">Liczba 1: </label>
-	<input id="id_x" type="text" name="x" value="<?php if (isset($x)) print($x); ?>" /><br />
-	<label for="id_op">Operacja: </label>
-	<select name="op">
-		<option value="plus">+</option>
-		<option value="minus">-</option>
-		<option value="times">*</option>
-		<option value="div">/</option>
-	</select><br />
-	<label for="id_y">Liczba 2: </label>
-	<input id="id_y" type="text" name="y" value="<?php if (isset($y)) print($y); ?>" /><br />
-	<input type="submit" value="Oblicz" />
-</form>	
+    <label for="id_kwota">Kwota kredytu: </label>
+    <input id="id_kwota" type="text" name="kwota" value="<?php if (isset($kwota)) print($kwota); ?>" /><br />
+    <label for="id_lata">Okres kredytu (w latach): </label>
+    <input id="id_lata" type="text" name="lata" value="<?php if (isset($lata)) print($lata); ?>" /><br />
+    <label for="id_oprocentowanie">Oprocentowanie (w %): </label>
+    <input id="id_oprocentowanie" type="text" name="oprocentowanie" value="<?php if (isset($oprocentowanie)) print($oprocentowanie); ?>" /><br />
+    <input type="submit" value="Oblicz ratę" />
+</form>
 
 <?php
-//wyświeltenie listy błędów, jeśli istnieją
+// wyświetlenie listy błędów, jeśli istnieją
 if (isset($messages)) {
-	if (count ( $messages ) > 0) {
-		echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
-		foreach ( $messages as $key => $msg ) {
-			echo '<li>'.$msg.'</li>';
-		}
-		echo '</ol>';
-	}
+    if (count($messages) > 0) {
+        echo '<ol style="margin: 20px; padding: 10px 10px 10px 30px; border-radius: 5px; background-color: #f88; width:300px;">';
+        foreach ($messages as $msg) {
+            echo '<li>'.$msg.'</li>';
+        }
+        echo '</ol>';
+    }
 }
 ?>
 
-<?php if (isset($result)){ ?>
+<?php if (isset($rata)){ ?>
 <div style="margin: 20px; padding: 10px; border-radius: 5px; background-color: #ff0; width:300px;">
-<?php echo 'Wynik: '.$result; ?>
+<?php echo 'Miesięczna rata: '.$rata.' zł'; ?>
 </div>
 <?php } ?>
 
